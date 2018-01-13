1882ce935bd4a48e5e0731f005c7b5a73670964c
Kenneth Keyes <kkeyes1@cnm.edu>
2018-01-12 20:33:57 -0700

    Created PWP Directory Structure and milestone-1.php

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Personal Website Project</title>
+	</head>
+	<body>
+		<h1>Purpose, Audience, Goal</h1>
+		<p>Purpose: What would you like your PWP to help you accomplish? This is a high-level goal akin to a Mission Statement. -  Add this line to README</p>
+		<p>Audience: Who will be using or viewing your site? Who is this site designed for, and who is your audience comprised of?</p>
+		<p>Goal: This is should be a quantifiable goal if possible.</p>
+		<h1>Persona</h1>
+		<p>Name</p>
+		<p>A detailed description that includes demographic information such as: age, job or role, relevant technology being used, behaviors and attitudes, and the situation or scenario in which they will use your site.</p>
+		<p>Goal(s) regarding the use of your site.</p>
+	</body>
+</html>
