42a58bc7dd52d67e6757a25b6006b35d7c7fada7
Kenneth Keyes <kkeyes1@cnm.edu>
2018-01-17 14:02:40 -0700

    Added purpose, audience and goal Added Persona

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -2,16 +2,28 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
+		<link rel="stylesheet" href="../css/milestone-stylesheet.css">
 		<title>Personal Website Project</title>
 	</head>
 	<body>
-		<h1>Purpose, Audience, Goal</h1>
-		<p>Purpose: What would you like your PWP to help you accomplish? This is a high-level goal akin to a Mission Statement. -  Add this line to README</p>
-		<p>Audience: Who will be using or viewing your site? Who is this site designed for, and who is your audience comprised of?</p>
-		<p>Goal: This is should be a quantifiable goal if possible.</p>
-		<h1>Persona</h1>
-		<p>Name</p>
-		<p>A detailed description that includes demographic information such as: age, job or role, relevant technology being used, behaviors and attitudes, and the situation or scenario in which they will use your site.</p>
-		<p>Goal(s) regarding the use of your site.</p>
+		<main class="content-main col-xs-12 col-md-9">
+			<div class="well __web-inspector-hide-shortcut__">
+				The following document is intended to serve as an example of what is expected in your PWP Milestone documentation. <strong>Your own projects and Milestones are expected to differ</strong>, but please use this as a guide.
+			</div>
+			<h2>PWP Milestone 1</h2>
+			<h3>Purpose, Audience &amp; Goal</h3>
+			<p><strong>Purpose</strong>: I will use this site as a touch point for potential employers, partners, and for networking. The site will most likely be used to gauge my particular set of skills as it relates to web development - though I intend to showcase my ability to pivot and explore new technologies and skill sets in order demonstrate that I can be a dynamic contributor to a team. If possible, I would like to make sure it is not too industry specific, and accesible for potential clients as well.</p>
+			<p><strong>Audience</strong>: This site is designed with a professional audience in mind, and is targeted to individuals within the web development and I.T. community nationwide. Target audience to include: startup founders, hiring managers, recruiters, and peers.</p>
+			<p><strong>Goal</strong>: _CHANGE_ME</p>
+			<h3>Persona</h3>
+			<p><strong>Name: </strong>Brandon Trebitowski<br><strong>Age: </strong>32</p>
+			<p><strong>Profession: </strong>CEO Pixegon/Launched his first company when he was 27, consolidated his company recently in 2015. </p>
+			<blockquote>Pixegon is not just your typical code factory. We enjoy helping established companies, startups and individuals build meaningful software for their businesses. The dedication, care, and innovation of our employees is what make Pixegon a recognized leader in large-scale mobile and backend application design, development, delivery and support. <cite>https://www.pixegon.com/about/</cite></blockquote>
+			<p><strong>Technology: </strong>: Primary: Apple Laptop, Apple iPhone</p>
+			<p>Secondary: Android mobile devices</p>
+			<p><strong>Attitudes and Behaviors: </strong>Minimalist at heart. Does not want to be bogged down by “overhead.” Likes things to be streamlined. Uses strict structure to manage family and work life. Usually on the move, meeting with clients, or spending time with family. While a more serious interest may yield a thorough inspection of the site, he will primarily view work related content on one of his phones first.</p>
+			<p><strong>Frustrations and Needs: </strong>Continues to be bothered by poor loading times and web-first designed sites. While he has seen some design that he likes, their function is not on par. He does not like this imbalance.</p>
+			<p><strong>Goals: </strong>Brandon is looking for a contractor to help out on a large “ground-up” project and has heard through mutual connections that I have an “impressive” online portfolio…</p>
+			<p><strong>User Story: </strong>As a user,  Brandon wants to be able to quickly review my credentials on a mobile device (either iPhone or android) and reach out to me instantaneously.</p>
 	</body>
 </html>
