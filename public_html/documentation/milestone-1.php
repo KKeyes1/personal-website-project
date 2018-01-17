1cd5d673426b28b30f1b9efd69d2ba591c472b61
Kenneth Keyes <kkeyes1@cnm.edu>
2018-01-17 15:20:48 -0700

    Added purpose, audience and goal Added Persona

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -2,27 +2,26 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
-		<link rel="stylesheet" href="../css/milestone-stylesheet.css">
 		<title>Personal Website Project</title>
 	</head>
 	<body>
-		<main class="content-main col-xs-12 col-md-9">
-			<div class="well __web-inspector-hide-shortcut__">
-			</div>
+		<div class="milestone">
 			<h2>PWP Milestone 1</h2>
+			<hr />
 			<h3>Purpose, Audience &amp; Goal</h3>
-			<p><strong>Purpose</strong>: I will use this site as a touch point for potential employers, partners, and for networking. The site will most likely be used to gauge my particular set of skills as it relates to web development - though I intend to showcase my ability to pivot and explore new technologies and skill sets in order demonstrate that I can be a dynamic contributor to a team. If possible, I would like to make sure it is not too industry specific, and accesible for potential clients as well.</p>
-			<p><strong>Audience</strong>: This site is designed with a professional audience in mind, and is targeted to individuals within the web development and I.T. community nationwide. Target audience to include: startup founders, hiring managers, recruiters, and peers.</p>
-			<p><strong>Goal</strong>: _CHANGE_ME</p>
+			<p><strong>Purpose</strong>: This website will be a Digital Resume that highlights my personal strengths and unique set of skills. I believe my particular goals/skills fit best with a very small start-up project and will build my site accordingly.</p>
+			<p><strong>Audience</strong>: I will use this site as a touch point for potential employers, partners, and for networking. The site will most likely be used to gauge my particular set of skills as it relates to web development - though I intend to showcase my ability to pivot and explore new technologies and skill sets in order demonstrate that I can be a dynamic contributor to a team. If possible, I would like to make sure it is not too industry specific, and accesible for potential clients as well.</p>
+			<p><strong>Goal</strong>: I want to appear well versed in front and backend development. I would like this site to encourage people to open up a dialogue with me, whether it be for employment opportunies, consulting, or just to ask me about my passions/hobbies.</p>
 			<h3>Persona</h3>
-			<p><strong>Name: </strong>Brandon Trebitowski<br><strong>Age: </strong>32</p>
-			<p><strong>Profession: </strong>CEO Pixegon/Launched his first company when he was 27, consolidated his company recently in 2015. </p>
-			<blockquote>Pixegon is not just your typical code factory. We enjoy helping established companies, startups and individuals build meaningful software for their businesses. The dedication, care, and innovation of our employees is what make Pixegon a recognized leader in large-scale mobile and backend application design, development, delivery and support. <cite>https://www.pixegon.com/about/</cite></blockquote>
-			<p><strong>Technology: </strong>: Primary: Apple Laptop, Apple iPhone</p>
-			<p>Secondary: Android mobile devices</p>
-			<p><strong>Attitudes and Behaviors: </strong>Minimalist at heart. Does not want to be bogged down by “overhead.” Likes things to be streamlined. Uses strict structure to manage family and work life. Usually on the move, meeting with clients, or spending time with family. While a more serious interest may yield a thorough inspection of the site, he will primarily view work related content on one of his phones first.</p>
-			<p><strong>Frustrations and Needs: </strong>Continues to be bothered by poor loading times and web-first designed sites. While he has seen some design that he likes, their function is not on par. He does not like this imbalance.</p>
-			<p><strong>Goals: </strong>Brandon is looking for a contractor to help out on a large “ground-up” project and has heard through mutual connections that I have an “impressive” online portfolio…</p>
-			<p><strong>User Story: </strong>As a user,  Brandon wants to be able to quickly review my credentials on a mobile device (either iPhone or android) and reach out to me instantaneously.</p>
+			<p><strong>Name</strong>: Brandon Trebitowski</p>
+			<p><strong>Age</strong>: 32</p>
+			<p><strong>Profession</strong>: CEO Pixegon/Launched his first company when he was 27, consolidated his company recently in 2015. </p>
+			<blockquote>Pixegon is not just your typical code factory. We enjoy helping established companies, startups and individuals build meaningful software for their businesses. The 			dedication, care, and innovation of our employees is what make Pixegon a recognized leader in large-scale mobile and backend application design, development, delivery and 			support. <cite>https://www.pixegon.com/about/</cite></blockquote>
+			<p><strong>Technology</strong>: Primary: Apple Laptop, Apple iPhone. Secondary: Android mobile devices</p>
+			<p><strong>Attitudes and Behaviors</strong>: Minimalist at heart. Does not want to be bogged down by “overhead.” Likes things to be streamlined. Uses strict structure to manage 			family and work life. Usually on the move, meeting with clients, or spending time with family. While a more serious interest may yield a thorough inspection of the site, he will 			primarily view work related content on one of his phones first.</p>
+			<p><strong>Frustrations and Needs</strong>: Continues to be bothered by poor loading times and web-first designed sites. While he has seen some design that he likes, their 			function is not on par. He does not like this imbalance.</p>
+			<p><strong>Goals</strong>: Brandon is looking for a contractor to help out on a large “ground-up” project and has heard through mutual connections that I have an “impressive” 			online portfolio…</p>
+			<p><strong>User Story</strong>: As a user,  Brandon wants to be able to quickly review my credentials on a mobile device (either iPhone or android) and reach out to me 			instantaneously.</p>
+		</div>
 	</body>
 </html>
