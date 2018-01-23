9f4280b1dc6cf30169e2f5c275d1a9f6d62bf3a3
Kenneth Keyes <kkeyes1@cnm.edu>
2018-01-22 19:37:36 -0700

    rewrote my content strategy based on updated wireframe. imported and linked mobile and web png

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -9,11 +9,17 @@
 			<h2>PWP Milestone 2</h2>
 			<hr />
 			<h3>Content Strategy</h3>
-			<p><strong>Landing Page</strong>: My landing page will mostly be a visual space, but will include a list of my best professional abilities and an immediate call to action to contact me or continue to view some references for those professional qualifications.</p>
+			<p><strong>Landing Page</strong>: My landing page will mostly be a visual space, but will have the title to my page "who is ken" a bar on the side and arrows at the bottom of the page indicating a scroll</p>
+			<p><strong>About Me</strong>: This will have a personal "why" statement, that encourages the user to find out how I have put my why into my work.</p>
 			<p><strong>Work</strong>: This will highlight some of my predominate strengths, like teaching/coaching, critical thinking, and adaptability as well as previews of some of my work, like the capstone project, my personal webpage, graphic design work, and physical jobs, like carpentry, engineering/design etc.</p>
+			<p><strong>Personal Passions</strong>: This will be a very high-level look into some of my other passions (hobbies) that are a large part of who I am. I will use custom iconography here.</p>
 			<p><strong>Contact</strong>: A simple, secure contact page that will allow a visitor to send me an email after entering their name and email address. On mobile I would like to have for the user to open up their message app and send me a text message.</p>
 			<h3>Wireframe</h3>
-			<p>I intend to build my site with a Flat UI. I may need to develop a mobile specific interface, as I want the mobile site to be almost exclusive accessible by swiping right to left and left to right, with relevant buttons at the bottom center of the screen. The desktop version of the site will take advantage of the extra real-estate by allowing for collapsible panels, and large, beautiful imagery to be the backdrop for the site.</p>
+			<p>In a perfect world... The background of my site is responsive, as the user scrolls the scene in the background changes (really just a set of 4 pictures that transition between each other) On the right hand side is where the content container lives. The container will be transparent and the text or images set apart through high-contrast. The content is "scroll" or have some other type of transition that mirrors the transition of the scene. The portfolio will be a simple gallery with captions that lets the user select a small image and blow it up and read the caption, as well as click the image to link to a site or other resource if need be. The personal passions page will have some custom iconography to highlight my hobbies that are important to who I am as a person, by clicking on each hobby I would like a drop down text to populate with some information about the hobby itself, and some informaton about how and why it is important to me. These collapsible windows should automatically collapse if another icon is pressed. Finally, the contact button will be under the content on every page and if pressed will bring up a pop-up window that will allow the user to enter their name and email and send me a secure message. Links to my githug, stackoverflow, linkedIn, etc. will also be accessible on this pop up. On mobile I would like to have the option to open and send and SMS message as well. All the links will also live in the footer of the page.</p>
+		</div>
+		<div>
+			<img src="mobile.png" alt="my mobile wireframe">
+			<img src="web.png" alt="my web wireframe">
 		</div>
 	</body>
 </html>
