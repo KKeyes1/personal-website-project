d4b784d385a2478afefbf885b80159644954fdfe
Kenneth Keyes <kkeyes1@cnm.edu>
2018-01-24 06:21:09 -0700

    Update: re-wrote content strategy removed mobile.png removed web.png added wireframe.png updated image src

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -9,17 +9,19 @@
 			<h2>PWP Milestone 2</h2>
 			<hr />
 			<h3>Content Strategy</h3>
-			<p><strong>Landing Page</strong>: My landing page will mostly be a visual space, but will have the title to my page "who is ken" a bar on the side and arrows at the bottom of the page indicating a scroll</p>
-			<p><strong>About Me</strong>: This will have a personal "why" statement, that encourages the user to find out how I have put my why into my work.</p>
-			<p><strong>Work</strong>: This will highlight some of my predominate strengths, like teaching/coaching, critical thinking, and adaptability as well as previews of some of my work, like the capstone project, my personal webpage, graphic design work, and physical jobs, like carpentry, engineering/design etc.</p>
-			<p><strong>Personal Passions</strong>: This will be a very high-level look into some of my other passions (hobbies) that are a large part of who I am. I will use custom iconography here.</p>
-			<p><strong>Contact</strong>: A simple, secure contact page that will allow a visitor to send me an email after entering their name and email address. On mobile I would like to have for the user to open up their message app and send me a text message.</p>
+			<p>Here is a loose structure for the content of my site. This is paired down to the bare-essentials that will be present no matter what. I have tried to design the site so that it can "expand" in areas that may develop more content as the bootcamp progresses, or over time in my career. The slider, for example, should never have more than 5 screens, but it is a dynamic container that I can change out as I see fit to portray new projects or characteristics of my professional persona.</p>
+			<hr />
+			<p><strong>Landing Page</strong>: Full Screen responsive image with site nav/menu</p>
+			<p><strong>About Me</strong>: Quote about ideas, followed by my personal why, followed by my personal and professional strengths and weaknesses. Large blurb on desktop with a full screen responsive background image. On mobile the blurb will display only the quote unless the container is expanded by the user.</p>
+			<p><strong>Portfolio</strong>: This will be a slider. A full-size, responsive image as well as a full-size container will give me the flexibility to convey a lot of meaning visually. For example, for my capstone project, I will be able to include icons to demonstrate what software technologies were implemented to complete the project. There will also be a container on the right that can have more thorough documentation. On mobile, this container will appear at the bootom of the screen. I may choose to write a simpler blurb to display on smaller screens, or allow for another expandable container.
+			</p>
+			<p><strong>Contact</strong>: There will be a live contact form on my site that will send a secure message to my email. This will also display relevant information like my current working location, email and "business" phone. On mobile this extra information will be part of an expandible menu to keep screen clutter down. Also on mobile I would like to include the functionality of a user being able to send an SMS message by clicking the phone number.
+			</p>
 			<h3>Wireframe</h3>
-			<p>In a perfect world... The background of my site is responsive, as the user scrolls the scene in the background changes (really just a set of 4 pictures that transition between each other) On the right hand side is where the content container lives. The container will be transparent and the text or images set apart through high-contrast. The content is "scroll" or have some other type of transition that mirrors the transition of the scene. The portfolio will be a simple gallery with captions that lets the user select a small image and blow it up and read the caption, as well as click the image to link to a site or other resource if need be. The personal passions page will have some custom iconography to highlight my hobbies that are important to who I am as a person, by clicking on each hobby I would like a drop down text to populate with some information about the hobby itself, and some informaton about how and why it is important to me. These collapsible windows should automatically collapse if another icon is pressed. Finally, the contact button will be under the content on every page and if pressed will bring up a pop-up window that will allow the user to enter their name and email and send me a secure message. Links to my githug, stackoverflow, linkedIn, etc. will also be accessible on this pop up. On mobile I would like to have the option to open and send and SMS message as well. All the links will also live in the footer of the page.</p>
+			<p>Built using balsamiq.</p>
 		</div>
 		<div>
-			<img src="mobile.png" alt="my mobile wireframe">
-			<img src="web.png" alt="my web wireframe">
+			<img src="wireframe.png" alt="my PWP wireframe built on balsamiq">
 		</div>
 	</body>
 </html>
