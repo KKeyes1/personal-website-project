4e3248bb8a17cc30041255ed33bffe66ab8e0631
Ken Keyes <kkeyes1@cnm.edu>
2018-02-11 08:15:56 -0700

    got my lines to "accelerate" and start 1 after the other!

--- public_html/JS/canvas.js
+++ public_html/JS/canvas.js
@@ -4,41 +4,54 @@ var canvas = document.querySelector("canvas");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+
 var c = canvas.getContext("2d");
 
 
-function staticLines(xStaticLine, yStaticLine, x1StaticLine, y1StaticLine) {
+
+// Static Line Object that takes two sets of x and y coordinates and draws perspective lines and draws them on the screen
+
+function StaticLines(xStaticLine, yStaticLine, x1StaticLine, y1StaticLine) {
 
 	this.xStaticLine = xStaticLine;
 	this.yStaticLine = yStaticLine;
 	this.x1StaticLine = x1StaticLine;
 	this.y1StaticLine = y1StaticLine;
+
 	this.draw = function () {
 
 		c.beginPath();
 		c.moveTo(this.xStaticLine, this.yStaticLine);
 		c.lineTo(this.x1StaticLine, this.y1StaticLine);
-		c.strokeStyle = "#333";
+		c.strokeStyle = "#38B2E7";
 		c.stroke();
 
 	}
 
 }
 
-
+// where I build my array of lines to draw on the screen
+// an empty array to populate
 var verticalLines = [];
+// number of vertical lines
 var numberOfVerticalLines = 15;
-var depth = 0.3;
+// percentage of the screen that the "horizon" will take up
+var depth = 0.01;
 
+
+//loop that creates the array of lines with varying x and y coordinates
 for (i = 0; i < numberOfVerticalLines + 1; i++) {
 
 	var xStaticLine = ((innerWidth / numberOfVerticalLines) * i ) + 0;
 	var yStaticLine = innerHeight;
 	var x1StaticLine = (((innerWidth * depth) / numberOfVerticalLines) * i) + (innerWidth - (innerWidth * depth)) / 2;
 	var y1StaticLine = innerHeight / 2;
-	verticalLines.push(new staticLines(xStaticLine, yStaticLine, x1StaticLine, y1StaticLine));
+	verticalLines.push(new StaticLines(xStaticLine, yStaticLine, x1StaticLine, y1StaticLine));
 
 }
+
+
+// Star Object that takes  positional coordinates x, y and directional sppeds dx, dy and a size radius, in order to draw the stars on the screen, and/or to update their direction when they collide with the edges of the window
 
 
 
@@ -52,10 +65,10 @@ function Star(x, y, dx, dy, radius) {
 
 	this.draw = function() {
 
-		c.fillStyle = "#c82124";
+		c.fillStyle = "#38B2E7";
 		c.beginPath();
 		c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-		c.strokeStyle = "blue";
+		c.strokeStyle = "#38B2E7";
 		c.stroke();
 		c.fill();
 
@@ -85,11 +98,17 @@ for(i = 0; i < numberOfStars; i++) {
 	var radius = Math.random() * 1;
 	var x = Math.random() * (innerWidth - radius * 2) + radius;
 	var y = Math.random() * innerHeight / 2;
-	var dx = (Math.random() - 0.5);
-	var dy = (Math.random() - 0.5);
+	var dx = (Math.random() - 0.5) / 10;
+	var dy = (Math.random() - 0.5) / 10;
 	starArray.push(new Star(x, y, dx, dy, radius));
 
 }
+
+
+
+// Line Object that accepts two x coordinates and one y that is updated to move the lone down the screen, and a value to control that speed dyLine
+
+
 
 function Line(xLine, yLine, x1Line, dyLine) {
 
@@ -97,32 +116,38 @@ function Line(xLine, yLine, x1Line, dyLine) {
 	this.yLine = yLine;
 	this.x1Line = x1Line;
 	this.dyLine = dyLine;
+	this.gravity = .1;
 
 	this.drawLine = function () {
-
 
 		c.beginPath();
 		c.moveTo(this.xLine, this.yLine);
 		c.lineTo(this.x1Line, this.yLine);
-		c.strokeStyle = "#333";
+		c.strokeStyle = "#38B2E7";
 		c.stroke();
 
 	}
 
 	this.updateLine = function() {
 
-		if (this.yLine > innerHeight) {
+		if (this.dyLine < 0) {
 
-			lineArray.push(new Line(0, innerHeight / 2, innerWidth, this.dyLine));
+			this.dyLine += this.gravity;
 
-			lineArray.shift();
+			return;
 
-			}
+			} else if (this.yLine > innerHeight) {
+
+			this.yLine = innerHeight / 2;
+			this.dyLine = 0;
+
+		}
+
+		this.dyLine += this.gravity;
 
 		this.yLine += this.dyLine;
 
 		this.drawLine();
-
 
 	}
 
@@ -130,22 +155,30 @@ function Line(xLine, yLine, x1Line, dyLine) {
 
 
 var lineArray = [];
-var numberOfLines = 1;
+var numberOfLines = 10;
 
 for (i = 0; i < numberOfLines; i++) {
 
-	var xLine =  0;
-	var yLine = (((innerHeight / 2) / numberOfLines) * i ) + (innerHeight / 2);
+	var xLine = 0;
+	var yLine = (innerHeight / 2);
 	var x1Line = innerWidth;
-	var dyLine = 4;
+	var dyLine = -i;
 	lineArray.push(new Line(xLine, yLine, x1Line, dyLine));
-
 }
+
+
+// animate functions for objects on the screen
 
 function animate() {
 
 	requestAnimationFrame(animate);
 	c.clearRect(0, 0, innerWidth, innerHeight);
+
+	c.beginPath();
+	c.moveTo(0, (innerHeight / 2));
+	c.lineTo(innerWidth, (innerHeight / 2));
+	c.strokeStyle = "#38B2E7";
+	c.stroke();
 
 	for (i = 0; i < starArray.length; i++) {
 
@@ -158,7 +191,6 @@ function animate() {
 function animateLine() {
 
 	requestAnimationFrame(animateLine);
-	// c.clearRect(0, 0, innerWidth, innerHeight);
 
 	for (i = 0; i < lineArray.length; i++) {
 
@@ -180,6 +212,8 @@ function drawVerticalLines() {
 
 }
 
+
+// calling all three animate functions starts it off
 animate();
 animateLine();
 drawVerticalLines();
--- public_html/index.php
+++ public_html/index.php
@@ -3,9 +3,10 @@
 	<head>
 		<meta charset="UTF-8">
 		<title>Who Is Ken</title>
+		<link rel="stylesheet" href="CSS/style.css">
 	</head>
 	<body>
-		<canvas></canvas>
+		<canvas id="landing-page"></canvas>
 		<script type="text/javascript" src="JS/canvas.js"></script>
 	</body>
 </html>
